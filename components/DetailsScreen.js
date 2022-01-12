0680ba025b7ce6a1bb2975d4a5a4a9fd3cdac3c3
undazurevska <95476474+undazurevska@users.noreply.github.com>
2022-01-12 12:18:39 +0200

    Update DetailsScreen.js

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, Card, Text, Image, Linking, View, StyleSheet, FlatList } from 'react-native';
+import Constants from 'expo-constants';
+import { List } from 'react-native-paper';
 
-export default function App() {
+
+export default function DetailsScreen ({navigation, route}) {
+  const {image, title, description, content, url, author} = route.params;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style = {{fontSize: 20, fontWeight: "bold", textAlign: "center", margin: 10}}> {JSON.stringify(title)} </Text> 
+      <Image 
+          style={styles.image}
+          source={{uri: image}}
+      />
+      <Text style ={styles.paragraph}>Description: {JSON.stringify(description)}</Text>
+      <Text style ={styles.paragraph}>Content: {JSON.stringify(content)}</Text>
+      <Text style ={styles.paragraph}>Author: {JSON.stringify(author)}</Text>
+      <Text style ={styles.paragraph}>URL: {JSON.stringify(url)}</Text>
+      <Button
+        title= 'Open the full article'
+        onPress={() => Linking.openURL(url) }>
+      </Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image: {
+    margin: 8,
+    width: 300,
+    height: 200,
+    alignSelf: 'center',
+    resizeMode: 'cover',
+    
   },
+ 
+  paragraph: {
+    margin: 8,
+    fontSize: 16,
+    textAlign: 'justify',
+  }
 });
