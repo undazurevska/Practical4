c2a23f9ef97c94818676555aeb94bf451da4ba0e
initalegzdina <95502251+initalegzdina@users.noreply.github.com>
2022-01-12 12:14:34 +0200

    Update HomeScreen.js

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Button, FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { List } from 'react-native-paper';
 
-export default function App() {
+export default function HomeScreen(
+  {navigation}) {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const renderTheFlatListItem = ({item}) => (
+  <List.Item
+      style = {styles.box}
+      title = <Text style={styles.paragraph}>{item.title}</Text>
+      onPress={() => {navigation.navigate ("DetailsScreen", 
+                                           {image: item.urlToImage,
+                                            title: item.title, 
+                                            description: item.description, 
+                                            content: item.content, 
+                                            url: item.url, author: 
+                                            item.author})} }
+  />
+);
+
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=API_KEY')
+      .then((response) => response.json())
+      .then((json) => setData(json.articles))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#f0f8ff", padding:10}}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ url }, index) => url}
+          renderItem={renderTheFlatListItem}
+        />
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  paragraph: {
+    fontSize: 16, 
+    fontWeight: "bold", 
+    padding: 14,
+    flex: 1, 
+    flexWrap: 'wrap'
   },
+
+  box: {
+    width: '95%',
+    height: 100,
+    alignSelf: 'center',
+    backgroundColor: '#fff',
+    margin: 8
+  }
 });
